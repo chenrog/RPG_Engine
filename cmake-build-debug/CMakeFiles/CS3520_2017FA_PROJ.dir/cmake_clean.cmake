d9bc05f5b0bcc8fc75d7dc1c1651180500566586
Roger <chen.rog@husky.neu.edu>
2017-11-06 21:51:05 -0500

    MakeLists up and running.

---
+++ cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS3520_2017FA_PROJ.dir/BattleMode.cpp.o"
+  "CMakeFiles/CS3520_2017FA_PROJ.dir/IUnit.cpp.o"
+  "CMakeFiles/CS3520_2017FA_PROJ.dir/main.cpp.o"
+  "CMakeFiles/CS3520_2017FA_PROJ.dir/Map.cpp.o"
+  "CS3520_2017FA_PROJ.pdb"
+  "CS3520_2017FA_PROJ"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS3520_2017FA_PROJ.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
