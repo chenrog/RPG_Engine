1fb7c06a304f40f7a2400075f17c63d10af210e0
andrewalc <andrewalc98@gmail.com>
2017-11-08 18:09:43 -0500

    AEntity start

--- cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/DependInfo.cmake
@@ -4,13 +4,15 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/whyar/Documents/GitHub/CS3520-2017FA-PROJ/main.cpp" "C:/Users/whyar/Documents/GitHub/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/main.cpp.obj"
-  "C:/Users/whyar/Documents/GitHub/CS3520-2017FA-PROJ/model/Equipment.cpp" "C:/Users/whyar/Documents/GitHub/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/model/Equipment.cpp.obj"
-  "C:/Users/whyar/Documents/GitHub/CS3520-2017FA-PROJ/model/Map.cpp" "C:/Users/whyar/Documents/GitHub/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/model/Map.cpp.obj"
-  "C:/Users/whyar/Documents/GitHub/CS3520-2017FA-PROJ/model/Units/EnemyUnit.cpp" "C:/Users/whyar/Documents/GitHub/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/model/Units/EnemyUnit.cpp.obj"
-  "C:/Users/whyar/Documents/GitHub/CS3520-2017FA-PROJ/model/Units/IUnit.cpp" "C:/Users/whyar/Documents/GitHub/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/model/Units/IUnit.cpp.obj"
-  "C:/Users/whyar/Documents/GitHub/CS3520-2017FA-PROJ/model/Units/NPCUnit.cpp" "C:/Users/whyar/Documents/GitHub/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/model/Units/NPCUnit.cpp.obj"
-  "C:/Users/whyar/Documents/GitHub/CS3520-2017FA-PROJ/model/Units/PlayerUnit.cpp" "C:/Users/whyar/Documents/GitHub/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/model/Units/PlayerUnit.cpp.obj"
+  "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/main.cpp" "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/main.cpp.obj"
+  "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/model/Equipment.cpp" "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/model/Equipment.cpp.obj"
+  "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/model/Map.cpp" "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/model/Map.cpp.obj"
+  "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/model/Point.cpp" "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/model/Point.cpp.obj"
+  "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/model/Units/AEntity.cpp" "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/model/Units/AEntity.cpp.obj"
+  "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/model/Units/EnemyUnit.cpp" "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/model/Units/EnemyUnit.cpp.obj"
+  "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/model/Units/IUnit.cpp" "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/model/Units/IUnit.cpp.obj"
+  "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/model/Units/NPCUnit.cpp" "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/model/Units/NPCUnit.cpp.obj"
+  "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/model/Units/PlayerUnit.cpp" "C:/Users/Andrew Alcala/CLionProjects/CS3520-2017FA-PROJ/cmake-build-debug/CMakeFiles/CS3520_2017FA_PROJ.dir/model/Units/PlayerUnit.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
---
+++ model/Units/AEntity.h
@@ -0,0 +1,35 @@
+//
+// Created by Andrew Alcala on 11/8/2017.
+//
+
+#ifndef CS3520_2017FA_PROJ_AENTITY_H
+#define CS3520_2017FA_PROJ_AENTITY_H
+
+
+#include "../Point.h"
+
+/**
+ * Class representing an Entity.
+ */
+class AEntity {
+public:
+    AEntity();
+
+    virtual ~AEntity();
+
+    virtual Point getPosition() = 0;
+
+    virtual bool isVisible() = 0;
+
+    virtual void enableVisibility() = 0;
+
+    virtual void disableVisibility() = 0;
+
+protected:
+    // The entity position in the overworld.
+    Point position;
+    bool visible;
+};
+
+
+#endif //CS3520_2017FA_PROJ_AENTITY_H
