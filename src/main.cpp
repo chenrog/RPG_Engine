2342e3606e754ba6d775cd9be9212f8d384f8559
Roger <chen.rog@husky.neu.edu>
2017-11-30 15:13:25 -0500

    Main Updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include "SDL2/SDL.h"
 
-
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 int main( int argc, char* args[] ) {
 
-    SDL_Window *window;                    // Declare a pointer
+    SDL_Window *window;                    // Declare a pointer to the window
 
     SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
 
 // Create an application window with the following settings:
     window = SDL_CreateWindow(
-            "AR-CUBED RPG",                  // window title
+            "AR-CUBED RPG",                    // window title
             SDL_WINDOWPOS_UNDEFINED,           // initial x position
             SDL_WINDOWPOS_UNDEFINED,           // initial y position
             SCREEN_WIDTH,                      // width, in pixels
@@ -32,18 +31,31 @@ int main( int argc, char* args[] ) {
     bool is_running = true;
     SDL_Event event;
     while (is_running) {
+            /* Poll for events. SDL_PollEvent() returns 0 when there are no  */
+            /* more events on the event queue, our while loop will exit when */
+            /* that occurs.                                                  */
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                is_running = false;
+            switch(event.type) {
+                // quit out of the game
+                case SDL_QUIT:
+                    is_running = false;
+
+                case SDL_KEYDOWN:
+                    printf( "Key press detected\n" );
+                break;
+
+                case SDL_KEYUP:
+                    printf( "Key release detected\n" );
+                break;
             }
         }
         SDL_Delay(16);
     }
 
-// Close and destroy the window
+    // Close and destroy the window
     SDL_DestroyWindow(window);
 
-// Clean up
+    // Clean up
     SDL_Quit();
     return 0;
 }
