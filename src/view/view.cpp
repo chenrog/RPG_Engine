78953a87b5c084bf4f50b0233df232e750de6ab1
Roger <chen.rog@husky.neu.edu>
2017-11-29 23:38:54 -0500

    View init.

---
+++ src/view/view.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Roger Chen on 11/27/17.
+//
+
+#include "view.h"
---
+++ src/view/view.h
@@ -0,0 +1,16 @@
+#ifndef CS3520_2017FA_PROJ_VIEW_H
+#define CS3520_2017FA_PROJ_VIEW_H
+
+#include "SDL.h"
+#include "../model/World/World.h"
+
+class view {
+public:
+    draw_world
+private:
+    World world;
+
+};
+
+
+#endif //CS3520_2017FA_PROJ_VIEW_H
