34838859a7a41fe4510a05566d5edc39e6ff3ab1
Ryan Teng <ryanrteng@gmail.com>
2017-12-05 15:03:53 -0500

    initialized player in world properly

--- test/WorldTest.h
+++ test/WorldTest.h
@@ -14,31 +14,29 @@ class WorldTest : public CxxTest::TestSuite {
 
     testConstructor() {
         World * world = new World();
-        TS_ASSERT_EQUALS(world.getWorldMap().getWorldMap().size(), 30);
-        TS_ASSERT_EQUALS(world.getWorldMap().getWorldMap()[0].size(), 30);
-        TS_ASSERT_EQUALS(world.getPlayer().getName(), "Twen");
-        TS_ASSERT_EQUALS(world.curMenuOption, 0);
-        TS_ASSERT_EQUALS(world.getCurrentGameState(), OVERWORLD);
+        TS_ASSERT_EQUALS(world->getPlayer()->getName(), "Twen");
+        TS_ASSERT_EQUALS(world->curMenuOption, 0);
+        TS_ASSERT_EQUALS(world->getCurrentGameState(), OVERWORLD);
 
     }
 
     testMovePlayer() {
-        TS_ASSERT_EQUALS(world.getPlayer().getPosition().getX(), 0);
-        TS_ASSERT_EQUALS(world.getPlayer().getPosition().getY(), 0);
         World * world = new World();
-        world.movePlayer(RIGHT, 1);
-        TS_ASSERT_EQUALS(world.getPlayer().getPosition().getX(), 1);
-        TS_ASSERT_EQUALS(world.getPlayer().getPosition().getY(), 0);
+        TS_ASSERT_EQUALS(world->getPlayer()->getPosition()->getX(), 0);
+        TS_ASSERT_EQUALS(world->getPlayer()->getPosition()->getY(), 0);
+        world->movePlayer(RIGHT);
+        TS_ASSERT_EQUALS(world->getPlayer()->getPosition()->getX(), 1);
+        TS_ASSERT_EQUALS(world->getPlayer()->getPosition()->getX(), 0);
     }
 
     testAddToInventory() {
         Item * item = new Item("IT", "EM");
         Item * meti = new Item("Back", "Wards");
-        World world = new World();
-        world.addToInventory(item);
-        TS_ASSERT_EQUALS(world.getInventory()[0].getName(), "IT");
-        world.addToInventory(meti);
-        TS_ASSERT_EQUALS(world.getInventory()[1].getName(), "Back");
+        World * world = new World();
+        world->addToInventory(item);
+        TS_ASSERT_EQUALS(world->getInventory()[0]->getName(), "IT");
+        world->addToInventory(meti);
+        TS_ASSERT_EQUALS(world->getInventory()[1]->getName(), "Back");
     }
 
 
